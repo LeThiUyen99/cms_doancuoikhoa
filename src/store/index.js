dbb91ef25bc9562e2cd03ac517badc9dede66541
kiendt12640 <83637597+LeThiUyen99@users.noreply.github.com>
2023-02-15 16:32:12 +0700

    .

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
-import { websocketPlugin } from '../utils/socket'
 
 Vue.use(Vuex)
 
@@ -20,8 +19,7 @@ const modules = modulesFiles.keys().reduce((modules, modulePath) => {
 
 const store = new Vuex.Store({
   modules,
-  getters,
-  plugins: [websocketPlugin]
+  getters
 })
 
 export default store
